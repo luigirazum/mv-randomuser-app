e7e425a778d967bc8d88f687ad3c54fa86483e44
Luis Zubia <soyzubia@gmail.com>
2023-04-10 07:41:54 -0700

    Add predefined selectors for users state

---
+++ src/redux/users/usersSelectors.js
@@ -0,0 +1,4 @@
+export const selectUsersStore = (store) => store.users;
+export const selectUsers = (store) => store.users.users;
+export const selectIsLoading = (store) => store.users.isLoading;
+export const selectError = (store) => store.users.error;
