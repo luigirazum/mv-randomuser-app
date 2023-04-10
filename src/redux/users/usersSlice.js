f2a727a9c90101533d40319caeea22d008cbed6f
Luis Zubia <soyzubia@gmail.com>
2023-04-09 23:10:51 -0700

    Create usersSlice && export the reducer as default && set initialState

---
+++ src/redux/users/usersSlice.js
@@ -0,0 +1,15 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {
+  users: [],
+  isLoading: false,
+  error: undefined,
+};
+
+const usersSlice = createSlice({
+  name: 'users',
+  initialState,
+  extraReducers: {},
+});
+
+export default usersSlice.reducer;
